2363fabf27c27cb828b2a32073420b67accbd02e
Oscar Aguayo <43186229+DcOscar@users.noreply.github.com>
2019-05-25 13:33:19 -0400

    Add files via upload
    Creación cascaron

---
+++ src/logico/GestionadorClub.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logico;
+
+import java.util.*;
+
+/**
+ *
+ * @author Oscar
+ */
+public class GestionadorClub {
+
+    private ArrayList<Boxeadora> grupoAzul;
+    private ArrayList<Boxeadora> grupoDorado;
+
+    public GestionadorClub() {
+        this.grupoAzul = new ArrayList<Boxeadora>();
+        this.grupoDorado = new ArrayList<Boxeadora>();
+    }
+
+    public void agregarBoxeadoraGrupoAzul(double altura) {
+
+    }
+
+    public void agregarBoxeadoraGrupoDorado(double altura) {
+  
+    }
+
+    private double compararAltura() {
+     
+    }
+
+    private boolean verificadorComparadorAltura(double comparador) {
+     
+    }
+
+    public String ordenarBoxeadoras() {
+       
+    }
+
+    public void mostrarGrupoAzul() {
+   
+
+    }
+
+    public void mostrarGrupoDorado() {
+    
+
+    }
+}
